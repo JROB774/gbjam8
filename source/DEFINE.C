21b5165dcce8b9c2e06cdeb02589786e40d12d74
Joshua Robertson <jrobertson774@yahoo.com>
2020-08-29 14:53:36 +0100

    Improved general-purpose defines.

--- source/DEFINE.C
+++ source/DEFINE.C
@@ -1,23 +1,26 @@
 /*////////////////////////////////////////////////////////////////////////////*/
 
-#define SCREEN_WIDTH  0x14 /* 20 */
-#define SCREEN_HEIGHT 0x12 /* 18 */
+#define SCREEN_WIDTH        0xA0   /* (160) Width of the screen in pixels.    */
+#define SCREEN_HEIGHT       0x90   /* (144) Height of the screen in pixels.   */
 
-#define TILE_WIDTH    0x08 /*  8 */
-#define TILE_HEIGHT   0x08 /*  8 */
+#define SCREEN_TILES_WIDTH  0x14   /* ( 20) Width of the screen in tiles.     */
+#define SCREEN_TILES_HEIGHT 0x12   /* ( 18) Height of the screen in tiles.    */
 
-#define BOOL          BOOLEAN
+#define TILE_WIDTH          0x08   /* (  8) Width of a tile in pixels.        */
+#define TILE_HEIGHT         0x08   /* (  8) Height of a tile in pixels.       */
 
-#define  U8           UINT8
-#define U16           UINT16
-#define U32           UINT32
-#define  S8           INT8
-#define S16           INT16
-#define S32           INT32
+#define BOOL                BOOLEAN
 
-#define INTERNAL      static
-#define GLOBAL        static
+#define  U8                 UINT8
+#define U16                 UINT16
+#define U32                 UINT32
+#define  S8                 INT8
+#define S16                 INT16
+#define S32                 INT32
 
-GLOBAL U8             i,j,k;
+#define INTERNAL            static
+#define GLOBAL              static
+
+GLOBAL U8                   i,j,k; /* General-purpose counter variables.      */
 
 /*////////////////////////////////////////////////////////////////////////////*/
