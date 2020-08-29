f70d7386a90ec58a6d8ffbe256b0a25045125fbf
Joshua Robertson <jrobertson774@yahoo.com>
2020-08-29 12:24:43 +0100

    Added font and room tilesets and the room map layout.

--- source/MAIN.C
+++ source/MAIN.C
@@ -1,3 +1,5 @@
+/*////////////////////////////////////////////////////////////////////////////*/
+
 #include <gb/gb.h>
 
 #include "DEFINE.C"
@@ -5,7 +7,23 @@
 #include "FADE.C"
 #include "INPUT.C"
 
+#include "TILESETS.C"
+#include "TILEMAPS.C"
+
 void main ()
 {
-    // ...
+    set_bkg_data(TILESET_FONT_OFFSET,TILESET_FONT_LENGTH,TILESET_FONT);
+    set_bkg_data(TILESET_ROOM_OFFSET,TILESET_ROOM_LENGTH,TILESET_ROOM);
+
+    set_bkg_tiles(0,0, TILEMAP_ROOM_WIDTH,TILEMAP_ROOM_HEIGHT,TILEMAP_ROOM);
+
+    DISPLAY_ON;
+    SHOW_BKG;
+
+    while (TRUE)
+    {
+        wait_vbl_done();
+    }
 }
+
+/*////////////////////////////////////////////////////////////////////////////*/
