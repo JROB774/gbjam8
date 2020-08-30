b956e994f4cb360cb70fe0c9560d2f85c9cdbc2a
Joshua Robertson <jrobertson774@yahoo.com>
2020-08-30 15:37:48 +0100

    Improved the fade effect quality.

--- source/FADE.C
+++ source/FADE.C
@@ -1,6 +1,6 @@
 /*////////////////////////////////////////////////////////////////////////////*/
 
-#define FADE_STEP_AMOUNT 0x04 /* How many unique steps in the fade process. */
+#define FADE_STEP_AMOUNT 0x07 /* (7) How many steps in the fade process. */
 
 INTERNAL void fade_to_white (U8 wait_period)
 {
@@ -8,9 +8,12 @@ INTERNAL void fade_to_white (U8 wait_period)
     for (i=0; i<FADE_STEP_AMOUNT; ++i) {
         switch (i) {
             case (0): BGP_REG = 0xE4; break;
-            case (1): BGP_REG = 0x90; break;
-            case (2): BGP_REG = 0x40; break;
-            case (3): BGP_REG = 0x00; break;
+            case (1): BGP_REG = 0xA4; break;
+            case (2): BGP_REG = 0x94; break;
+            case (3): BGP_REG = 0x54; break;
+            case (4): BGP_REG = 0x50; break;
+            case (5): BGP_REG = 0x40; break;
+            case (6): BGP_REG = 0x00; break;
         }
         for (j=0; j<wait_period; ++j) {
             wait_vbl_done();
@@ -25,8 +28,11 @@ INTERNAL void fade_from_white (U8 wait_period)
         switch (i) {
             case (0): BGP_REG = 0x00; break;
             case (1): BGP_REG = 0x40; break;
-            case (2): BGP_REG = 0x90; break;
-            case (3): BGP_REG = 0xE4; break;
+            case (2): BGP_REG = 0x50; break;
+            case (3): BGP_REG = 0x54; break;
+            case (4): BGP_REG = 0x94; break;
+            case (5): BGP_REG = 0xA4; break;
+            case (6): BGP_REG = 0xE4; break;
         }
         for (j=0; j<wait_period; ++j) {
             wait_vbl_done();
@@ -40,9 +46,12 @@ INTERNAL void fade_to_black (U8 wait_period)
     for (i=0; i<FADE_STEP_AMOUNT; ++i) {
         switch (i) {
             case (0): BGP_REG = 0xE4; break;
-            case (1): BGP_REG = 0xF9; break;
-            case (2): BGP_REG = 0xFE; break;
-            case (3): BGP_REG = 0xFF; break;
+            case (1): BGP_REG = 0xE5; break;
+            case (2): BGP_REG = 0xE9; break;
+            case (3): BGP_REG = 0xEA; break;
+            case (4): BGP_REG = 0xFA; break;
+            case (5): BGP_REG = 0xFE; break;
+            case (6): BGP_REG = 0xFF; break;
         }
         for (j=0; j<wait_period; ++j) {
             wait_vbl_done();
@@ -57,8 +66,11 @@ INTERNAL void fade_from_black (U8 wait_period)
         switch (i) {
             case (0): BGP_REG = 0xFF; break;
             case (1): BGP_REG = 0xFE; break;
-            case (2): BGP_REG = 0xF9; break;
-            case (3): BGP_REG = 0xE4; break;
+            case (2): BGP_REG = 0xFA; break;
+            case (3): BGP_REG = 0xEA; break;
+            case (4): BGP_REG = 0xE9; break;
+            case (5): BGP_REG = 0xE5; break;
+            case (6): BGP_REG = 0xE4; break;
         }
         for (j=0; j<wait_period; ++j) {
             wait_vbl_done();
