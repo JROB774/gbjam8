809c5b6acdbf05686aa25debb188b8992fea6939
Joshua Robertson <jrobertson774@yahoo.com>
2020-09-01 00:40:51 +0100

    Made it so stdio.h is now included in DEBUG_MODE.

--- source/DEBUG.C
+++ source/DEBUG.C
@@ -3,6 +3,7 @@
 /* Some debugging utilities for the BGB emulator if we're in DEBUG_MODE. */
 #if DEBUG_MODE
 #include <gb/bgb_emu.h>
+#include <stdio.h>
 #define DEBUG_PROFILE_BEGIN(msg) BGB_PROFILE_BEGIN(msg        )
 #define DEBUG_PROFILE_END(  msg) BGB_PROFILE_END  (msg        )
 #define DEBUG_LOG_MESSAGE(  ...) BGB_MESSAGE_FMT  (__VA_ARGS__)
